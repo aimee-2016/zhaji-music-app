b378bdf1e187c848774717d7c3e1c427257de48c
lhj <2420479720@qq.com>
2019-09-11 21:49:06 +0800

    第二章准备工作

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    hellow aimee
   </div>
 </template>
 
@@ -11,13 +12,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import 'common/stylus/variable.styl'
+#app
+  color: $color-theme
 </style>
